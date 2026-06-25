445f464a8eacc0e19d34225a9b937d2fc3cb807c
NikoHolm <niko.holm@edu.sakky.fi>
2026-06-26 00:34:44 +0300

    refactor: simplify menu logic with case

--- linux-check.sh
+++ linux-check.sh
@@ -2,13 +2,13 @@
 
 showStorage() {
   echo "You have this much storage left" 
-  df -h /
+  df -h / | awk 'NR==2 {print "Storage information" "\n Free: " $4"  \n Total: " $2 " \n Used: " $5 }'
   read -p "Press Enter to continue..."
 }
 
 showRam() {
   echo "You have ram left:"
-  free -h
+  free -h | awk 'NR==2 {print "RAM information" "\n Free: " $4"  \n Total: " $2 " \n Used: " $3 }'
   read -p "Press Enter to continue..."
 }
 
@@ -19,8 +19,7 @@ showKernel() {
 }
 
 showUptime() {
-  echo "Uptime"
-  uptime
+  uptime -p | sed 's/^up //'
   read -p "Press Enter to continue..."
 }
 
@@ -40,6 +39,8 @@ This will update your entire system."
 showExit() {
   exit
 }
+
+#starts loop
 
 while true; do
 
@@ -62,30 +63,17 @@ echo "4. Uptime"
 echo "5. Update your system"
 echo "6. Exit"
 
-#starts loop
+
 
 
 read -p "Press number 1-6: " input
-
-if [ "$input" -eq 1 ]; then
-    showStorage
-elif [ "$input" -eq 2 ]; then
-    showRam
-elif [ "$input" -eq 3 ]; then
-    showKernel
-elif [ "$input" -eq 4 ]; then
-    showUptime
-elif [ "$input" -eq 5 ]; then
-    showUpdate
-elif [ "$input" -eq 6 ]; then
-    showExit
-
-else 
-    echo "Invalid choice. Please enter a number between 1 and 6."
-    read -p "Press Enter to continue..."
-fi
-done    
-
-
-
-
+case $input in 
+  1) showStorage ;;
+  2) showRam ;;
+  3) showKernel ;;
+  4) showUptime ;;
+  5) showUpdate ;;
+  6) showExit ;;
+  *) echo "Invalid input. Please enter a number between 1 and 6." ;;
+esac
+done
